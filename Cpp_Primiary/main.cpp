8e134434f8d45c39d6b3f04d25626e178c2f7da5
konglee <konglee873@gmail.com>
2016-11-25 09:16:16 +0800

    Review

--- Cpp_Primiary/main.cpp
+++ Cpp_Primiary/main.cpp
@@ -7,9 +7,85 @@
 //
 
 #include <iostream>
+using namespace std;
+/* task-2
+ typedef int kl_int;
+ 
+ 
+ */
+
+/*
+ task-3
+ enum color
+{
+    blue,
+    green,
+    yellow
+}a;
+ */
+
+int getResult(int a, int b);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     std::cout << "Hello, World!\n";
+    
+    /*
+     task-1
+    int i = 1;
+    i ++;
+    cout << "i = " << i << "美丽" << endl;
+    */
+    
+    /*
+     task-2
+    kl_int a = 5;
+    kl_int result = a * 10;
+    cout << "result is  " << result << endl;
+    */
+    
+    /*
+     task-3
+     a = green;
+    
+    switch (a)
+    {
+        case blue:
+        {
+            cout << "color is " << a << endl;
+        }
+            break;
+        case yellow:
+        {
+            cout << "color is " << a << endl;
+        }
+        case green:
+        {
+            cout << "color is " << a << endl;
+        }
+            
+        default:
+            break;
+    }
+     */
+    
+    /*task-4
+     int c = getResult(5, 4);
+    
+    cout << "c = " << c << endl;
+     
+     */
+    
+    
+    
     return 0;
 }
+
+/*
+ task-4
+ int getResult(int a, int b)
+{
+    return a * b;
+}
+ */
+
